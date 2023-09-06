6ab8fe13b111a3c1fb46dc09e3f45c60c1c012fa
Sebastian Rojo <arpagon@gmail.com>
2023-09-05 20:19:13 -0500

    FIx type

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 
+export const runtime = 'edge';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -46,7 +48,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Artisian Carrer AI{' '}
+            Artisan Carrer AI{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -62,7 +64,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Artisian Debate AI{' '}
+            Artisan Debate AI{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -78,13 +80,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            ArtisianAI English Tutor{' '}
+            ArtisanAI English Tutor{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            ArtisianAI English Tutor! is your English Tutor.
+            ArtisanAI English Tutor! is your English Tutor.
           </p>
         </a>
       </div>
