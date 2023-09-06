ad2ec91078910ddbd4f4651a85d13898ca6cdef1
Sebastian Rojo <arpagon@gmail.com>
2023-09-06 09:05:08 -0500

    ADD better image and fix ligth theme

--- app/page.tsx
+++ app/page.tsx
@@ -31,11 +31,11 @@ export default function Home() {
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-          src="https://github.com/ArtisanLabs/.github/raw/main/profile/assets/images/banners/arpagon__voice_waveforms_diagram_outline_an_illustration_on_bla_6c9d4e43-c8a1-42a3-b53c-c5038c961667_clipdrop-enhance.webp"
-          alt="Artisan Labs Logo"
-          width={500}
-          height={500}
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] light:invert"
+          src="https://raw.githubusercontent.com/ArtisanLabs/.github/main/profile/assets/images/banners/transparent/arpagon__voice_waveforms_diagram_complete_city.webp"
+          alt="ArtisanLabs City"
+          width={1260}
+          height={630}
           priority
         />
       </div>
